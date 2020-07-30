88e62bbefeb3bf3d4b48e6fc764ebfb9ff4aca7c
januario86 <januario86fp@gmail.com>
2020-07-30 00:28:26 -0300

    Criação de Entities e Enums

---
+++ src/main/java/com/januario/pontointeligente/api/enums/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.januario.pontointeligente.api.enums;
+
+public enum PerfilEnum {
+
+	ROLE_ADMIIN,
+	ROLE_USUARIO;
+	
+}
---
+++ src/main/java/com/januario/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,12 @@
+package com.januario.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	
+	INICIO_TRABALO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA
+
+}
